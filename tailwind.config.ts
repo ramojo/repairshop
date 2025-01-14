ca370908f40607fe3df4765963e2df6ad4c6c2ba
Allan Ramogo <amramogo@gmail.com>
2025-01-14 23:31:33 +0300

    Add animations to tailwinf config for layout and template

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,7 +59,30 @@ export default {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			keyframes: {
+				'appear': {
+					from: {
+						opacity: '0',
+					},
+					to: {
+						opacity: '1',
+					}
+				},
+				'slide': {
+					from: {
+						'transform': 'translateX(100%)',
+					},
+					to: {
+						'transform': 'translateX(0%)',
+					}
+				}
+			},
+			animation: {
+				'appear': 'appear 1s ease-in-out',
+				'slide': 'slide 759ms ease-in-out',
+			},
 		}
 	},
+	// eslint-disable-next-line @typescript-eslint/no-require-imports
 	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
